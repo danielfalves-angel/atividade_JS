3974f49aa92b9267730117531a0f25a9a7c4a828
DanielAlves <daniel_f_alves@estudante.sesisenai.org.br>
2026-03-25 11:32:40 -0300

    add - ex_04 completa

--- scripts/ex_04.js
+++ scripts/ex_04.js
@@ -1,3 +1,8 @@
 function quatroValores(){
-    
+    let numeros = [];
+    for(i = 0; i < 4; i++){
+        numeros[i] = parseInt(prompt("digte um numero"));
+    }
+    numeros.sort((a,b) => b - a);
+    alert(numeros);
 }
