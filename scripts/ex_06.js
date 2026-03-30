d5eafbbba175576a32788155dfe6b6e859f3d99e
DanielAlves <daniel_f_alves@estudante.sesisenai.org.br>
2026-03-30 07:36:53 -0300

    test - ex_06

--- scripts/ex_06.js
+++ scripts/ex_06.js
@@ -1,4 +1,9 @@
 function vogalConso() {
     let letra = (prompt("digite uma letra:"));
     alert (letra);
+    if (letra = a || letra = e || letra = i || letra = o || letra = u) {
+        alert("essa letra é vogal");
+    } else {
+        alert("essa letra é consoante");
+    }
 }
