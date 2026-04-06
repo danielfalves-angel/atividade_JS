5e3b617e78f554bdc7c43f7498cd49e9583bf331
danil_f_alves <daniel_f_alves@estudante.sesisenai.org.br>
2026-04-06 09:41:05 -0300

    up - ex_06 completo de verdade

--- scripts/ex_06.js
+++ scripts/ex_06.js
@@ -1,7 +1,6 @@
 function vogalConso() {
     let letra = (prompt("digite uma letra:"));
-    alert (letra);
-    if (letra = a && e && i && o && u) {
+    if (letra == "a" || letra == "e" || letra =="i" || letra =="o" || letra =="u") {
         alert("essa letra é vogal");
     } else {
         alert("essa letra é consoante");
